1516b0ec4fe934f47265455ed9baa52f2951b479
Stephen Martin <stephenlmartin@student.cofo.edu>
2015-02-02 20:33:43 -0600

    Fixed some bugs in the code.

--- src/artillery/Main.java
+++ src/artillery/Main.java
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-package artillery;
-
-public class Main {
-
-}
-=======
 package artillery;
 
 public class Main {
 	//THIS IS MY COMMENT!!!!!!11!!!
 	// This is my comment.
 }
->>>>>>> origin/untested
