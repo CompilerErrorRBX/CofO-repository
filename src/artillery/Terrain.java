fca9b34e88e6924a2ba3e903a424377097ec06ab
Stephen Martin <stephenlmartin@student.cofo.edu>
2015-02-03 13:09:07 -0600

    Worked on terrain generation.

--- src/artillery/Terrain.java
+++ src/artillery/Terrain.java
@@ -1,5 +1,6 @@
 package artillery;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import javax.media.opengl.GL2;
@@ -13,6 +14,34 @@ public class Terrain extends Base {
 	private int mapSize = 100;
 	private int index = 0;
 	
+	private Vector2i baseOffset = new Vector2i(0, 5);
+	
+	private ArrayList<Vector2i> fractal = new ArrayList<Vector2i>();
+	
+	private void createFractal(int fractures, float persistence) {
+		// Initialize fractal
+		fractal = new ArrayList<Vector2i>();
+		// Add a point on the left side of the window.
+		fractal.add(new Vector2i(0, baseOffset.y));
+		// Add a point on the right side of the window.
+		fractal.add(new Vector2i(mapSize, baseOffset.y));
+		for (int n = 0; n < fractures; n++) {
+			// Create fractures.
+			int nodes = fractal.size();
+			for (int i = 1; i < fractal.size(); i++) {
+				Vector2i node1 = fractal.get(i-1);
+				Vector2i node2 = fractal.get(i);
+				System.out.println((Math.random() * persistence) * 2 - persistence);
+				fractal.add(i, new Vector2i((node1.x + node2.x)/2, (int) (((node1.y + node2.y)/2) + (((Math.random() * persistence) * 2) - persistence))));
+				i++;
+			}
+			persistence /= 2;
+		}
+		for (Vector2i vec : fractal) {
+			System.out.println(vec);
+		}
+	}
+	
 	public void init(Window win, GLAutoDrawable drawable) {
 		System.out.println("Initilizing map");
 		GL2 gl = drawable.getGL().getGL2();
@@ -21,6 +50,9 @@ public class Terrain extends Base {
 				(mapSize / 2.0f) - 5);
 
 		index = gl.glGenLists(1);
+		
+		createFractal(5, 1);
+		
 		gl.glNewList(index, GL2.GL_COMPILE);
 		for (int i = 0; i < mapSize; i++) {
 			float x = 0;
