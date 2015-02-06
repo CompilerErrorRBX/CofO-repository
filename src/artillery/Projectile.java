76ba44c05e3b39a06b7a02adce24ca1f419be136
dmarler <daltonmarler@outlook.com>
2015-02-05 19:29:17 -0600

    Started a basic projectile class.

---
+++ src/artillery/Projectile.java
@@ -0,0 +1,22 @@
+package artillery;
+
+public class Projectile {
+
+	private static int initialVelocityXI = 0;
+	private static int initialVelocityYI = 0;
+	private static int initialHeightI = 0;
+	private static float initialVelocityXF = 0;
+	private static float initialVelocityYF = 0;
+	private static float initialHeightF = 0;
+	
+	
+	private static int accelerationYi = 0;
+	private static int accelerationXi = 0;
+	private static float accelerationYf = 0.0f;
+	private static float accelerationXf = 0.0f;
+	
+	//M/S
+	public final static int EARTH_GRAVITY_INT = 10;
+	public final static float EARTH_GRAVITY_FLOAT = 9.8f;
+	
+}
